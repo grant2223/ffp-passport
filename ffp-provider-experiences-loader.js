95d0d309e8619850323ba8d10d49b9c93aeb9f3a
grant2223 <grant@findfitpeople.com>
2026-06-01 15:15:43 +0400

    v3 Update ffp-provider-experiences-loader.js

--- ffp-provider-experiences-loader.js
+++ ffp-provider-experiences-loader.js
@@ -577,7 +577,7 @@
         : '');
 
     var foot =
-      (editing ? '<button class="btn btn-ghost left" onclick="confirmDeleteExperience(\'' + editing.id + '\'); closeModal()"><span class="ms">delete</span> Delete</button>' : '') +
+      (editing ? '<button class="btn btn-ghost left" onclick="confirmDeleteExperience(\'' + editing.id + '\')"><span class="ms">delete</span> Delete</button>' : '') +
       '<button class="btn btn-ghost" onclick="closeModal()">Cancel</button>' +
       '<button class="btn btn-pri" onclick="saveExperience(\'' + (editing ? editing.id : '') + '\')">' +
         (editing ? 'Save changes' : 'Submit for review') +
